ed7c72f4e70418c52314694581cfb8cbee0b351b
Wael Mchantaf <wael.mchantaf@bld.ai>
2023-08-26 02:35:24 +0300

    edit fenwick-tree.cpp

--- fenwick-tree.cpp
+++ fenwick-tree.cpp
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-class BIT {
+struct BIT {
   int sz; // TAKE CARE OF ZERO INDEX!!
   vector<int> pre;
 
-public:
   BIT(int X) {
     sz = X;
 
@@ -12,7 +11,6 @@ public:
     pre.resize(sz + 1, 0);
   }
 
-public:
   void update(int x, int val) {
     for (; x <= sz; x += (x & (-x)))
       pre[x] += val;
@@ -20,7 +18,6 @@ public:
     return;
   }
 
-public:
   int get(int x) {
     long long res = 0;
     for (; x > 0; x -= (x & (-x)))
